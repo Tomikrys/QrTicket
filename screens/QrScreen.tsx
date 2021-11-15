150ae64cee8cff7cf02647a9fab3c7754e81dbd6
Tomáš Vostřejž <T.vostrejz@gmail.com>
2021-11-15 19:58:07 +0100

    fix: blinking QR Scanner on navigation

--- screens/QrScreen.tsx
+++ screens/QrScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, useWindowDimensions, KeyboardAvoidingView, View } from 'react-native';
 
 import QrReader from './components/QrReader';
@@ -8,13 +8,33 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import TicketsScreen from '../screens/TicketsScreen';
 import ModalTicketValidator from './components/ModalTicketValidator';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function QrScreen({ticketType, markTicketAsUsed}) {
+  const [hasPermission, setHasPermission] = useState(false);
   const DrawerR = createDrawerNavigator();
   const windowWidth = useWindowDimensions().width;
 
+  useEffect(() => {
+    let permissionNeeded = true;
+
+    BarCodeScanner.requestPermissionsAsync().then(({status}) => {
+      if (permissionNeeded) setHasPermission(status  === 'granted');
+    });
+
+    return () => { permissionNeeded = false };
+  }, []);
+
+  if (!hasPermission) {
+    return (
+      <View style={{ height: '100%', justifyContent: 'center' }}>
+        <Text category='h4' style={{ textAlign: 'center' }}>Please allow Camera</Text>
+      </View>
+    );
+  }
+
   const Content = () => (
-    <QrScreenContent ticketType={ticketType} markTicketAsUsed={markTicketAsUsed}/>
+    <QrScreenContent ticketType={ticketType} markTicketAsUsed={markTicketAsUsed} hasPermission={hasPermission}/>
   )
 
   function CustomDrawerContent(props) {
@@ -36,10 +56,10 @@ export default function QrScreen({ticketType, markTicketAsUsed}) {
   );
 }
 
-function QrScreenContent({ticketType, markTicketAsUsed}) {
+function QrScreenContent({ticketType, markTicketAsUsed, hasPermission}) {
   return (
     <View style={styles.container}>
-      <QrReader itemToValidate={ticketType.key} markAsUsed={markTicketAsUsed}/>
+      <QrReader itemToValidate={ticketType.key} markAsUsed={markTicketAsUsed} hasPermission={hasPermission} />
       <ManualValidation />
       <ModalTicketValidator />
       <Text style={{fontSize: 30, width: '100%', textAlign: 'center', marginBottom: 30}}>{ticketType.title}</Text>
