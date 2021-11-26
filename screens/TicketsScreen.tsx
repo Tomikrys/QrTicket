1994b026abbcf433e95103aea1fda76ee970b76e
Tomáš Ryšavý <xrysav27@vutbr.cz>
2021-11-26 17:02:41 +0100

    ignore special chars + formatting

--- screens/TicketsScreen.tsx
+++ screens/TicketsScreen.tsx
@@ -5,7 +5,7 @@ import { TouchableWithoutFeedback, StyleSheet, View } from 'react-native';
 
 import ModalTicketEditor from './components/ModalTicketEditor';
 
-export default function TicketsScreen({onSelectTicket}) {
+export default function TicketsScreen({ onSelectTicket }) {
 
   const [editorVisible, setEditorVisible] = React.useState(false);
   const [selectedTicket, selectTicket] = React.useState(null);
@@ -13,40 +13,40 @@ export default function TicketsScreen({onSelectTicket}) {
   const [searchedTickets, setSearchedTickets] = React.useState(allTickets);
 
   const renderItemIcon = (props) => (
-    <Icon {...props} name='person'/>
+    <Icon {...props} name='person' />
   );
 
   const renderItemEdit = (props, item) => (
-    <Button size='medium' status='info' style={styles.editButton} onPress={() => {selectTicket(item); setEditorVisible(true);}}>Open in editor</Button>
+    <Button size='medium' status='info' style={styles.editButton} onPress={() => { selectTicket(item); setEditorVisible(true); }}>Open in editor</Button>
   );
 
   const renderItem = ({ item, index }) => (
-      <ListItem
-        key={index}
-        title={() => <Text style={styles.listItemTitle}>{item.name}</Text>}
-        description={item.ID}
-        accessoryLeft={renderItemIcon}
-        accessoryRight={(props) => isAdmin() ? renderItemEdit(props, item) : <></>}
-        onPress={() => {selectTicket(item); onSelectTicket(item);}}
-        style={styles.listItem}
-      />
+    <ListItem
+      key={index}
+      title={() => <Text style={styles.listItemTitle}>{item.name}</Text>}
+      description={item.ID}
+      accessoryLeft={renderItemIcon}
+      accessoryRight={(props) => isAdmin() ? renderItemEdit(props, item) : <></>}
+      onPress={() => { selectTicket(item); onSelectTicket(item); }}
+      style={styles.listItem}
+    />
   );
 
   return (
-    <View style={{flex:1}}>
-      <TopNavigation 
-        style={{elevation: 5}}
-        title={() => <Text style={{flex: 1, textAlign: 'center', fontSize: 20}}>List of tickets</Text>}
+    <View style={{ flex: 1 }}>
+      <TopNavigation
+        style={{ elevation: 5 }}
+        title={() => <Text style={{ flex: 1, textAlign: 'center', fontSize: 20 }}>List of tickets</Text>}
       />
-      <Divider/>
+      <Divider />
       <List
-              style={styles.list}
-              data={searchedTickets}
-              renderItem={renderItem}
-              ListEmptyComponent={<Text style={styles.emptyListText}>No tickets found!</Text>}
+        style={styles.list}
+        data={searchedTickets}
+        renderItem={renderItem}
+        ListEmptyComponent={<Text style={styles.emptyListText}>No tickets found!</Text>}
       />
       {(isAdmin() && selectedTicket != null) ? <ModalTicketEditor selectedTicket={selectedTicket} onClose={() => setEditorVisible(false)} visible={editorVisible} /> : <></>}
-      <Divider/>
+      <Divider />
       <View style={{ elevation: 5 }}>
         <SearchBar setSearchedTickets={setSearchedTickets} allTickets={allTickets} />
       </View>
@@ -54,41 +54,45 @@ export default function TicketsScreen({onSelectTicket}) {
   );
 };
 
-function SearchBar({setSearchedTickets, allTickets}) {
-    const [value, setValue] = React.useState('');
+function SearchBar({ setSearchedTickets, allTickets }) {
+  const [value, setValue] = React.useState('');
 
-    const clearSearchBar = () => {
-      setValue('');
+  const clearSearchBar = () => {
+    setValue('');
+    setSearchedTickets(allTickets);
+  };
+
+  const renderIcon = (props) => (
+    <TouchableWithoutFeedback onPress={clearSearchBar}>
+      <Icon {...props} name={'close-outline'} />
+    </TouchableWithoutFeedback>
+  );
+
+  const onChangeText = (nextValue) => {
+    setValue(nextValue);
+    if (nextValue == '') {
       setSearchedTickets(allTickets);
-    };
+    } else {
+      setSearchedTickets(allTickets.filter(
+        // ticket => removeDiacritics(ticket.name.toLowerCase()).includes(removeDiacritics(nextValue.toLowerCase())) ||
+        ticket => removeDiacritics(ticket.name.toLowerCase()).includes(removeDiacritics(nextValue.toLowerCase())) ||
+          ticket.ID.toLowerCase().includes(nextValue.toLowerCase()))
+      );
+    }
+  };
 
-    const renderIcon = (props) => (
-      <TouchableWithoutFeedback onPress={clearSearchBar}>
-        <Icon {...props} name={'close-outline'}/>
-      </TouchableWithoutFeedback>
-    );
-
-    const onChangeText = (nextValue) => {
-      setValue(nextValue);
-      if(nextValue == '') {
-        setSearchedTickets(allTickets);
-      } else {
-        setSearchedTickets(allTickets.filter(ticket => ticket.name.toLowerCase().includes(nextValue.toLowerCase()) || ticket.ID.toLowerCase().includes(nextValue.toLowerCase())));
-      }
-    };
-
-    return (
-      <Input
-        value={value}
-        autoCapitalize='none'
-        autoCorrect={false}
-        placeholder='Search'
-        style={styles.searchBar}
-        clearButtonMode='while-editing'
-        accessoryRight={renderIcon}
-        onChangeText={onChangeText}
-      />
-    );
+  return (
+    <Input
+      value={value}
+      autoCapitalize='none'
+      autoCorrect={false}
+      placeholder='Search'
+      style={styles.searchBar}
+      clearButtonMode='while-editing'
+      accessoryRight={renderIcon}
+      onChangeText={onChangeText}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
@@ -122,3 +126,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
 });
+
+
+function removeDiacritics(str) {
+  var defaultDiacriticsRemovalMap = [
+    { 'base': 'a', 'letters': /[áä]/g },
+    { 'base': 'c', 'letters': /[č]/g },
+    { 'base': 'd', 'letters': /[ď]/g },
+    { 'base': 'e', 'letters': /[ěé]/g },
+    { 'base': 'i', 'letters': /[í]/g },
+    { 'base': 'l', 'letters': /[ĺľ]/g },
+    { 'base': 'n', 'letters': /[ň]/g },
+    { 'base': 'o', 'letters': /[óô]/g },
+    { 'base': 'r', 'letters': /[řŕ]/g },
+    { 'base': 's', 'letters': /[š]/g },
+    { 'base': 't', 'letters': /[ť]/g },
+    { 'base': 'u', 'letters': /[ůú]/g },
+    { 'base': 'y', 'letters': /[ý]/g },
+    { 'base': 'z', 'letters': /[ž]/g },
+  ];
+  for (var i = 0; i < defaultDiacriticsRemovalMap.length; i++) {
+    str = str.replace(defaultDiacriticsRemovalMap[i].letters, defaultDiacriticsRemovalMap[i].base);
+  }
+  return str;
+}
