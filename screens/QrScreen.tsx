150c5a9f18bef3d5ccac6cecc393dd3c6a2849ae
Petr <creepyteams@email.cz>
2021-12-31 03:59:33 +0100

    Vertical position of manual validation box on focus of its input was fixed.

--- screens/QrScreen.tsx
+++ screens/QrScreen.tsx
@@ -37,7 +37,8 @@ export default function QrScreen({ ticketType, markTicketAsUsed, hasPermission, 
   const [modalState, setModalState] = useState<ModalState>({ type: 'HIDDEN', isVisible: false });
 
   const itemToValidate = ticketType.key;
-  const [manualValidationValue, setManualValidationValue] = React.useState('');
+  const [manualValidationInputFocus, setManualValidationInputFocus] = useState('80%');
+  const [manualValidationValue, setManualValidationValue] = useState('');
 
   // handler when barcode is scanned
   const handleBarCodeScanned = ({ type, data }: any) => {
@@ -149,7 +150,7 @@ export default function QrScreen({ ticketType, markTicketAsUsed, hasPermission, 
       />
 
       {/* Manual Validation */}
-      <View style={styles.manualValidationBox}>
+      <View style={[styles.manualValidationBox, {height: manualValidationInputFocus}]}>
         <Input
           value={manualValidationValue}
           autoCapitalize='characters'
@@ -163,6 +164,8 @@ export default function QrScreen({ ticketType, markTicketAsUsed, hasPermission, 
           // onFocus={clearManualValidationInput}
           onChangeText={nextValue => setManualValidationValue(nextValue)}
           onSubmitEditing={e => fetchAndDisplayModal(e.nativeEvent.text)}
+          onFocus={() => (setManualValidationInputFocus('70%'))}
+          onBlur={() => (setManualValidationInputFocus('80%'))}
         />
       </View>
 
